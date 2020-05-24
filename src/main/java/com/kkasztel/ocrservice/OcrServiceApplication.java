4b9fe3164524b931d55d351847c2367e84352328
Kacper Kasztelanic <kcpr51@hotmail.com>
2020-05-24 12:57:29 +0200

    Initial commit

---
+++ src/main/java/com/kkasztel/ocrservice/OcrServiceApplication.java
@@ -0,0 +1,12 @@
+package com.kkasztel.ocrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kkasztel/ocrservice/OcrServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kkasztel.ocrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
