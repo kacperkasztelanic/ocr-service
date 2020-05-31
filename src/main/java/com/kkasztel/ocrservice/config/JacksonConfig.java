2516caeb53f946e62b2b09b15d8ab198fb62f9a4
Kacper Kasztelanic <kcpr51@hotmail.com>
2020-05-31 17:01:24 +0200

    Add Jackson config

---
+++ src/main/java/com/kkasztel/ocrservice/config/JacksonConfig.java
@@ -0,0 +1,30 @@
+package com.kkasztel.ocrservice.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.vavr.jackson.datatype.VavrModule;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()//
+                .setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)//
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)//
+                .configure(SerializationFeature.INDENT_OUTPUT, true)//
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)//
+                .registerModule(new VavrModule())//
+                .registerModule(new JavaTimeModule())//
+                .registerModule(new Jdk8Module());
+    }
+}
