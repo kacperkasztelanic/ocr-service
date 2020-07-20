39b0f0e9fb13cf64ad80743f6d2c1519ddab1484
Kacper Kasztelanic <kcpr51@hotmail.com>
2020-07-20 21:18:13 +0200

    Add FileTypeProbeException

---
+++ src/main/java/com/kkasztel/ocrservice/exception/FileTypeProbeException.java
@@ -0,0 +1,8 @@
+package com.kkasztel.ocrservice.exception;
+
+public class FileTypeProbeException extends OcrException {
+
+    public FileTypeProbeException(Throwable cause) {
+        super(cause);
+    }
+}
--- src/main/java/com/kkasztel/ocrservice/web/OcrResource.java
+++ src/main/java/com/kkasztel/ocrservice/web/OcrResource.java
@@ -1,12 +1,15 @@
 package com.kkasztel.ocrservice.web;
 
+import com.kkasztel.ocrservice.exception.FileStorageException;
+import com.kkasztel.ocrservice.exception.FileTypeProbeException;
+import com.kkasztel.ocrservice.exception.OcrException;
 import com.kkasztel.ocrservice.messaging.JobQueue;
 import com.kkasztel.ocrservice.service.job.JobService;
-import com.kkasztel.ocrservice.service.result.ResultService;
-import com.kkasztel.ocrservice.service.uuid.UuidSupplier;
 import com.kkasztel.ocrservice.service.model.Job;
 import com.kkasztel.ocrservice.service.model.Result;
+import com.kkasztel.ocrservice.service.result.ResultService;
 import com.kkasztel.ocrservice.service.storage.FileService;
+import com.kkasztel.ocrservice.service.uuid.UuidSupplier;
 
 import org.apache.tika.config.TikaConfig;
 import org.apache.tika.metadata.Metadata;
@@ -15,6 +18,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +30,7 @@ import java.time.Clock;
 import java.time.Instant;
 
 import io.vavr.CheckedFunction1;
-import io.vavr.control.Try;
+import io.vavr.control.Either;
 import lombok.extern.slf4j.Slf4j;
 
 import static io.vavr.API.CheckedFunction;
@@ -71,31 +75,34 @@ public class OcrResource {
         String id = uuidSupplier.get();
         Instant now = clock.instant();
         return Try(file::getBytes)//
-                .flatMap(b -> fileService.save(b)//
-                        .toTry()//
-                        .flatMapTry(i -> probeFileExtension(b).map(e -> Tuple(i, e)))//
+                .toEither().mapLeft(FileStorageException::new).mapLeft(OcrException.class::cast)//
+                .flatMap(b -> fileService.save(b).mapLeft(OcrException.class::cast)//
+                        .flatMap(i -> probeFileExtension(b).mapLeft(OcrException.class::cast).map(e -> Tuple(i, e)))//
                         .map(p -> Job.of(id, now, p._1, p._2))//
                 )//
                 .map(jobService::save)//
                 .peek(jobQueue::enqueueJob)//
-                .mapTry(urlFunction)//
-                .onFailure(t -> log.error(t.getMessage(), t))//
+                .flatMap(j -> Try(() -> urlFunction.apply(j))//
+                        .toEither().mapLeft(OcrException::new)//
+                )//
+                .peekLeft(t -> log.error(t.getMessage(), t))//
                 .map(u -> accepted().body(u))//
                 .getOrElse(() -> status(HttpStatus.INTERNAL_SERVER_ERROR).build());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> check(String id) {
+    public ResponseEntity<String> check(@PathVariable String id) {
         return resultService.findById(id)//
                 .flatMap(Result::getResults)//
                 .map(ResponseEntity::ok)//
                 .getOrElse(() -> notFound().build());
     }
 
-    private Try<String> probeFileExtension(byte[] file) {
+    private Either<FileTypeProbeException, String> probeFileExtension(byte[] file) {
         return Try(() -> new ByteArrayInputStream(file))//
                 .mapTry(s -> tika.getMimeRepository().detect(s, new Metadata()))//
                 .mapTry(t -> tika.getMimeRepository().forName(t.toString()))//
-                .map(MimeType::getExtension);
+                .map(MimeType::getExtension)//
+                .toEither().mapLeft(FileTypeProbeException::new);
     }
 }
